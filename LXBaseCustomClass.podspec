0f95f2c70d122c470ef4becf08df933a2d4e3464
liuxia55 <32723746+liuxia55@users.noreply.github.com>
2019-04-23 09:22:59 +0800

    修改弹出框文件

---
+++ LXBaseCustomClass.podspec
@@ -0,0 +1,15 @@
+Pod::Spec.new do |s|
+s.name         = 'LXBaseCustomClass'
+s.version      = '0.0.1'
+s.summary      = 'BaseClass'
+s.description  = <<-DESC
+基础控制器 model button 提示框 弹框 textView占位 lable行间距 color转换
+DESC
+s.homepage     = 'https://github.com/liuxia55/LXBaseCustomClass.git'
+s.license= { :type => "MIT", :file => "LICENSE" }
+s.authors      = {'liuxia55' => '810357314@qq.com'}
+s.platform     = :ios, '8.0'
+s.source       = {:git => 'https://github.com/liuxia55/LXBaseCustomClass.git', :tag => s.version}
+s.source_files = 'LXBaseCustomClass/LXBaseCustomClass/**/*'
+s.requires_arc = true
+end
--- LXBaseCustomClass/LXBaseCustomClass/MessageAlert/UIView+MJAlertView.m
+++ LXBaseCustomClass/LXBaseCustomClass/MessageAlert/UIView+MJAlertView.m
@@ -7,14 +7,13 @@
 //
 
 #import "UIView+MJAlertView.h"
-#import "AppDelegate.h"
 #import <QuartzCore/QuartzCore.h>
 
 
 #define SCREEN_WIDTH        [UIScreen mainScreen].bounds.size.width
 #define SCREEN_HEIGHT       [UIScreen mainScreen].bounds.size.height
 
-#define AppDelegate ((AppDelegate *)[UIApplication sharedApplication].delegate)
+#define AppDelegate [UIApplication sharedApplication].delegate
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 //你要展示的背景颜色
